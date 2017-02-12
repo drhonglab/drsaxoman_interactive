3ed840361d5d0da7cd986430b00f2a08396cf202
Euy Shick Hong <drsax@naver.com>
2017-02-12 20:38:04 +0900

    Create ard.js

---
+++ ard_node/ard.js
@@ -0,0 +1,26 @@
+/* var SerialPort = require("serialport");
+var serialport = new SerialPort("/port c:usbmodem1421",{
+  baudrate: 9600,
+
+
+});
+serialport.on('open', function(){
+  console.log('Serial Port Opend');
+  serialport.on('data', function(data){
+      console.log(data[0]);
+  });
+});
+
+*/
+
+
+var SerialPort = require("serialport");
+//var serialport = new SerialPort("/dev/cu.usbserial-DA01MHA4");
+ var serialport = new SerialPort("/dev/cu.usbmodem1a21");
+serialport.on('open', function(){
+  console.log('Serial Port Opend');
+  serialport.on('data', function(data){
+     //data=data.toString('utf8');
+      console.log(data[0]);
+  });
+});
